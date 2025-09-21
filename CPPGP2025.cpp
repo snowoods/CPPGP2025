7fefce2f2f08a1112ce519b41398d60f03c0bf6f
nbc <nbc@snowoods.com>
2025-09-21 11:42:26 +0900

    feat: Add WinMain window.

--- CPPGP2025.cpp
+++ CPPGP2025.cpp
@@ -1,11 +1,85 @@
 // CPPGP2025.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
-
+#include <windows.h>
 #include <iostream>
 #include "ZVector3.h"
 #include "ZMatrix.h"
 
-int main()
+void matrixTest();
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_DESTROY: // 창이 닫힐 때 발생
+            PostQuitMessage(0); // 메시지 루프를 종료하도록 요청
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+}
+
+void SetupConsole()
+{
+    AllocConsole(); // 현재 프로세스에 새로운 콘솔 창을 할당
+    FILE* pConsole;
+    // stdout에 쓰는 모든 내용이 새로 할당된 콘솔 창에 출력되도록 세팅한다.
+    freopen_s(&pConsole, "CONOUT$", "w", stdout);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    SetupConsole();
+    matrixTest();
+
+    // 윈도우 클래스 정의
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"CPPGPClass";
+
+    // 윈도우 클래스 등록
+    RegisterClass(&wc);
+
+    // 윈도우 생성
+    HWND hWnd = CreateWindowEx(
+        0,                      // 확장 윈도우 스타일
+        L"CPPGPClass",          // 윈도우 클래스 이름
+        L"CPPGP",               // 윈도우 제목
+        WS_OVERLAPPEDWINDOW,    // 윈도우 스타일
+
+        // 크기 및 위치
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL, // 부모 윈도우
+        NULL, // 메뉴
+        hInstance,
+        NULL // 추가 애플리케이션 데이터
+    );
+
+    if (hWnd == NULL)
+    {
+        std::cout << "Failed to create window!" << std::endl;
+        return 0;
+    }
+
+    // 윈도우를 화면에 표시 및 업데이트
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    FreeConsole();
+
+    return (int)msg.wParam;
+}
+
+void matrixTest()
 {
     ZVector3 v1(1, 2, 3);
     std::cout << v1 << std::endl;
@@ -45,7 +119,6 @@ int main()
     ZMatrix worldMatrix = matScale * matRotation * matTranslation;
     ZVector3 finalWorldPointByMatrix = localPoint.Transform(worldMatrix);
     std::cout << finalWorldPointByMatrix << std::endl;
-
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
