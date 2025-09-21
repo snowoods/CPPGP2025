f22127a4dc5c5388b2ddd3b486243aa3922ce625
nbc <nbc@snowoods.com>
2025-09-21 12:17:46 +0900

    feat: Add GameState.

--- CPPGP2025.cpp
+++ CPPGP2025.cpp
@@ -1,11 +1,18 @@
 // CPPGP2025.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
+#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #include <iostream>
+#include "GameState.h"
 #include "ZVector3.h"
 #include "ZMatrix.h"
+#include "CPPGP2025.h"
 
-void matrixTest();
+GameState* g_currentState = nullptr;
+HWND g_hWnd;
+
+void MatrixTest();
+void ChangeState(GameState* newState);
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -30,7 +37,7 @@ void SetupConsole()
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     SetupConsole();
-    matrixTest();
+    MatrixTest();
 
     // 윈도우 클래스 정의
     WNDCLASS wc = {};
@@ -79,7 +86,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return (int)msg.wParam;
 }
 
-void matrixTest()
+void MatrixTest()
 {
     ZVector3 v1(1, 2, 3);
     std::cout << v1 << std::endl;
@@ -121,13 +128,13 @@ void matrixTest()
     std::cout << finalWorldPointByMatrix << std::endl;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+void ChangeState(GameState* newState)
+{
+    if (g_currentState)
+    {
+        g_currentState->Exit();
+        delete g_currentState;
+    }
+    g_currentState = newState;
+    g_currentState->Enter(g_hWnd);
+}
---
+++ GameState.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class GameState
+{
+public:
+	virtual ~GameState() {}
+
+	virtual void Enter(HWND hWnd) = 0;
+	virtual void Exit() = 0;
+	virtual void Update(float elapsedTime) = 0;
+	virtual void Draw(HDC hdc) = 0;
+	virtual void OnKeyDown(WPARAM wParam) = 0;
+	virtual void OnKeyUp(WPARAM wParam) = 0;
+	virtual void OnMouseDown(int x, int y, int button) = 0;
+	virtual void OnMouseUp(int x, int y, int button) = 0;
+	virtual void OnMouseMove(int x, int y) = 0;
+};
